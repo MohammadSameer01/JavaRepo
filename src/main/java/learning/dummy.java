0613ca76362599ab519f29dd5a0d404d48346e0a
Mohammad Sameer <sammusmarty563@gmail>
2025-05-24 12:06:09 +0530

    deleted dummy.java

--- src/main/java/learning/dummy.java
+++
@@ -1,9 +0,0 @@
-package learning;
-
-public class dummy {
-    public static void main(String[] args) {
-        System.out.println("Mohammad");
-        System.out.println("Sameer");
-        System.out.println("Sammu");
-    }
-}
