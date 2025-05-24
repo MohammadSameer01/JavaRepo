4e321e58d2d161b7b7659548db8afb0cac5b08d8
Mohammad Sameer <sammusmarty563@gmail>
2025-05-24 12:04:57 +0530

    added two more sysout statements in dummy.java

--- src/main/java/learning/dummy.java
+++ src/main/java/learning/dummy.java
@@ -2,6 +2,8 @@ package learning;
 
 public class dummy {
     public static void main(String[] args) {
+        System.out.println("Mohammad");
         System.out.println("Sameer");
+        System.out.println("Sammu");
     }
 }
